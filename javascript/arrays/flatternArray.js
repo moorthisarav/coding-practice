1cfe1d85255816f5429f6f2583c47305627cf34c
ssu <moorthi.subramani@sigos.com>
2018-06-25 11:05:18 +0200

    updated flatternArray.ts

--- javascript/arrays/flatternArray.js
+++ javascript/arrays/flatternArray.js
@@ -5,13 +5,13 @@ console.log(input);
 var FlatternArray = /** @class */ (function () {
     function FlatternArray() {
     }
-    FlatternArray.prototype.flattern = function (array) {
+    //flatern array using recursion
+    FlatternArray.prototype.flatternRecursion = function (array) {
         var result = [];
         for (var i = 0; i < array.length; i++) {
             var arrVal = array[i];
             if (Array.isArray(arrVal)) {
-                console.log(result);
-                result = result.concat(this.flattern(arrVal));
+                result = result.concat(this.flatternRecursion(arrVal));
             }
             else {
                 result.push(arrVal);
@@ -19,8 +19,27 @@ var FlatternArray = /** @class */ (function () {
         }
         return result;
     };
+    FlatternArray.prototype.flatternUsingReduce = function (array) {
+        var self = this;
+        return array.reduce(function (a, b) {
+            if (Array.isArray(b)) {
+                return a.concat(self.flatternUsingReduce(b));
+            }
+            else {
+                return a.concat(b);
+            }
+        }, []);
+    };
+    FlatternArray.prototype.flatternUsingReduceUsingLamda = function (array) {
+        var self = this;
+        return array.reduce(function (a, b) { return a.concat(Array.isArray(b) ? self.flatternUsingReduceUsingLamda(b) : b); }, []);
+    };
     return FlatternArray;
 }());
 var flatternArray = new FlatternArray();
-console.log('output:');
-console.log(flatternArray.flattern(input));
+console.log('output recursion:');
+console.log(flatternArray.flatternRecursion(input));
+console.log('output array reduce:');
+console.log(flatternArray.flatternUsingReduce(input));
+console.log('output array reduce with lamda:');
+console.log(flatternArray.flatternUsingReduceUsingLamda(input));
--- javascript/arrays/flatternArray.ts
+++ javascript/arrays/flatternArray.ts
@@ -1,30 +1,56 @@
 
 //Flattern the given multi dimentional array to single array 
-let input: any = [1, 1, [3, [4, 5], 6], 7, [8]];
-console.log('input:');
-console.log(input);
+
 
 class FlatternArray {
-    
-    public flattern(array: any): any {
-        
+
+    //flatern array using recursion
+    flatternRecursion(array: any[]) {
+
         let result: any = [];
 
-        for(var i = 0; i < array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
             var arrVal = array[i];
-            if(Array.isArray(arrVal)) {
-              console.log(result);
-              result = result.concat(this.flattern(arrVal));
+            if (Array.isArray(arrVal)) {
+                result = result.concat(this.flatternRecursion(arrVal));
             } else {
-              result.push(arrVal);
+                result.push(arrVal);
             }
         }
-    return result;
+        return result;
     }
 
+    flatternUsingReduce(array: any[]): any {
+
+        let self = this;
+        
+        return array.reduce(function(a, b) {
+            if (Array.isArray(b)) {
+                return a.concat(self.flatternUsingReduce(b));
+            } else {
+               return a.concat(b);
+            }
+        }, []);
+    }
+
+    flatternUsingReduceUsingLamda(array: any[]): any {
+
+        let self = this;        
+        return array.reduce((a, b) => a.concat( Array.isArray(b) ? self.flatternUsingReduceUsingLamda(b) : b ), []);
+    }
 
 }
 
 var flatternArray = new FlatternArray();
-console.log('output:');
-console.log (flatternArray.flattern(input))
+
+let input = [1, 1, [3, [4, 5], 6], 7, [8]];
+console.log('input:');
+console.log(input);
+
+//Output
+console.log('output recursion:');
+console.log(flatternArray.flatternRecursion(input))
+console.log('output array reduce:');
+console.log(flatternArray.flatternUsingReduce(input))
+console.log('output array reduce with lamda:');
+console.log(flatternArray.flatternUsingReduceUsingLamda(input))
